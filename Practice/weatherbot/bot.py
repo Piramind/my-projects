9707a16e30da8fc98d2ba31daab320c5e0aca5bf
Lev <tikhonov.lev0@gmail.com>
2021-11-15 16:53:31 +0300

    update

--- Practice/weatherbot/bot.py
+++ Practice/weatherbot/bot.py
@@ -1,4 +1,20 @@
-input pyowm
+import pyowm
+
+owm =  pyowm.OWM('0522639eb5e28d6595fc5ac1ce46b4ca')
+
+place = input("В каком городе/стране?: ")
+mgr = owm.weather_manager()
+observation = mgr.weather_at_place('London,GB')
+#observation = mgr.weather_manager().weather_at_place(place)
+w = observation.get_weather()
+
+print("В городе " + place + " сейчас " + 
+w.get_detailed_status())
+temp = w.get_temperature('celsius')['temp']
+print('Температура в районе: ' + str(temp) + '°C')
+
+"""
+import pyowm
 
 owm = pyowm.OWM('', Language = "ru")
 
@@ -8,3 +24,4 @@ observation = owm.weather_at_place('London,GB')
 w = observation.get_weather()
 
 print(w)
+"""
