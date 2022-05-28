d10017b888ef92c293a2792e8a34d11ab618c20b
Lev Tikhonov <ltikhonov@cloudlinux.com>
2022-05-28 15:08:15 +0300

    update generator and skip password saving

--- generator.py
+++ generator.py
@@ -3,8 +3,8 @@ import os
 
 
 chars = "+-/*!&$#?=@<>abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
-number = int(input('Кол-во паролей'))
-leight = int(input('Кол-во символов'))
+number = int(input('Кол-во паролей: '))
+leight = int(input('Кол-во символов: '))
 
 for x in range(number):
     password = ""
@@ -12,9 +12,9 @@ for x in range(number):
     for i in range(leight):
         password += random.choice(chars)
     print(password)
-
+"""
     file = open( 'password.txt', 'a' )
     file.write('\n' + password)
     file.close()
-
+"""
 os.system('pause')
